842af75758664f8e1ccf10a69fbc475a5886e35a
Fur3k <florianh59320@gmail.com>
2024-03-21 10:57:37 +0100

    Update DynamicArray.cpp

--- Dossier Florian/DynamicArray/DynamicArray/DynamicArray.cpp
+++ Dossier Florian/DynamicArray/DynamicArray/DynamicArray.cpp
@@ -1,20 +1,65 @@
-// DynamicArray.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+// EXERCICE : Tableau Dynamique (Cf Python)
+
+typedef struct IntArray
+{
+	int* pContent;
+
+	int iSize;
+	int iCapacity;
+} IntArray;
+
+void Init(IntArray* pIntArray)
+{	
+	pIntArray->iSize = 0;
+	pIntArray->iCapacity = 5;
+
+	pIntArray->pContent = (int*)malloc(pIntArray->iCapacity * sizeof(int));
+	if (pIntArray->pContent == NULL)
+	{
+		exit(1);
+	}
+}
+
+void Add(IntArray* pIntArray, int iValue)
+{
+	for (int i = 0; i < pIntArray->iCapacity; i++) {
+		if (pIntArray->pContent[i] == NULL) {
+			pIntArray->pContent[i] = iValue;
+			exit(1);
+		}
+	}
+}
+
+void Insert(IntArray* pIntArray, int iValue, int iIndex)
+{
+	for (int i = pIntArray->iCapacity-1; i > iIndex; i--) {
+		if (!(pIntArray->pContent[i + 1] == pIntArray->pContent[i]) && !(pIntArray->pContent[i + 1] == NULL))
+		{
+			pIntArray->pContent[i + 1] = pIntArray->pContent[i];
+		}
+	}
+	pIntArray->pContent[iIndex] = iValue;
+}
+
+void Remove(IntArray* pIntArray, int iIndex)
+{
+	//
+}
+
+void Print(IntArray* pIntArray)
+{
+	//
+}
+
+void Destroy(IntArray* pIntArray)
+{
+	//
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
