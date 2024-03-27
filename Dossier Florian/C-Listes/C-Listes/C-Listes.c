98acb629d4b0c89c4aaf47ecd369accfd9b98b6b
Fur3k <florianh59320@gmail.com>
2024-03-27 12:27:42 +0100

    c-list

--- Dossier Florian/C-Listes/C-Listes/C-Listes.c
+++ Dossier Florian/C-Listes/C-Listes/C-Listes.c
@@ -1,20 +1,92 @@
-// C-Listes.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+typedef struct Node
+{
+    Node* pNext;
+    Node* pPrevious;
+
+    int iValue;
+} Node;
+
+typedef struct List
+{
+    Node* pFirst;
+
+    int iCount;
+} List;
+
+void Init(List* pList)
+{
+    //
+    pList->iCount = 0;
+    pList->pFirst = NULL;
+}
+
+void AddFirst(List* pList, int iValue)
+{
+    //
+    pList->iCount += 1;
+    pList->pFirst->iValue = iValue;
+    pList->pFirst->pNext = &pList[pList->iCount + 1];
+    pList->pFirst->pPrevious = &pList[pList->iCount - 1];
+}
+
+void AddLast(List* pList, int iValue)
+{
+    //
+    pList->iCount += 1;
+    pList->pFirst->iValue = iValue;
+    pList->pFirst->pNext = &pList[pList->iCount + 1];
+    pList->pFirst->pPrevious = &pList[pList->iCount - 1];
+}
+
+void AddAfterNode(List* pList, Node* pNode, int iValue)
+{
+    //
+}
+
+void Insert(List* pList, int iValue, int iIndex)
+{
+    //
+}
+
+void Remove(List* pList, int iIndex)
+{
+    //
+}
+
+void RemoveNode(List* pList, Node* pNode)
+{
+    //
+}
+
+Node* GetNode(List* pList, int iIndex)
+{
+    //
+    return pList[iIndex].pFirst;
+}
+
+void Print(List* pList)
+{
+    //
+}
+
+void Destroy(List* pList)
+{
+    //
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    List oList;
+    init(&oList);
+
+    AddLast(&oList, 1);
+    AddLast(&oList, 2);
+    AddLast(&oList, 3);
+
+    AddFirst(&oList, 0);
+
+    Destroy(&oList);
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
